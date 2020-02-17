e9fce9eedd503a1c7a94255c5f81a72a1f3adc29
Dirk Barbi <dirk.barbi@awi.de>
2020-02-17 17:04:08 +0100

    add_rc_entry

--- esm_tools/esm_tools.py
+++ esm_tools/esm_tools.py
@@ -1,18 +1,33 @@
 """Main module."""
-import os
+import os, sys
+    
+rcfile = os.path.expanduser("~") + "/.esmtoolsrc"
 
 def get_paths():
-    import os 
-    rcfile = os.path.expanduser("~") + "/.esmtoolsrc"
-
-    function_path = namelist_path = runscript_path = ""
-
-    with open(rcfile) as rc:
-        while line = rc.readline():
-            if line.startswith("FUNCTION"):
-                function_path = line.split("=", 1)[-1]
-            elif line.startswith("NAMELIST"):
-                namelist_path = line.split("=", 1)[-1]
-            elif line.startswith("RUNSCRIPT"):
-                runscript_path = line.split("=", 1)[-1]
+    function_path = read_rc_entry("FUNCTION_PATH")
+    namelist_path = read_rc_entry("NAMELIST_PATH")
+    runscript_path = read_rc_entry("RUNSCRIPT_PATH")
     return function_path, namelist_path, runscript_path
+
+def write_rc_entry(key, value):
+    all_lines = [key + "=" + value]
+
+    if os.path.isfile(rcfile):
+        with open(rcfile) as rc:
+            while line = rc.readline():
+                if not key == line.split("=", 1)[0]:
+                    all_lines.append(line)
+        os.remove(rcfile)
+
+    with open(rcfile, "w") as rc:
+        for line in all_lines:
+                rc.write(line)
+
+def read_rc_entry(key):
+    if os.path.isfile(rcfile):
+        with open(rcfile) as rc:
+            while line = rc.readline():
+                if line.split("=", 1)[0] == key.upper():
+                    return line.split("=", 1)[1]
+    print (rcfile + " not found, exiting")
+    sys.exit(-1)

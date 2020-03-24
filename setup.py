a44256c49a310615280ed30094ac9d20abad2472
Paul Gierz <pgierz@awi.de>
2020-03-24 11:41:57 +0100

    pre-bumpversion fixup

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dbarbi/esm_master',
-    version='3.1.2',
+    version='0.1.0',
     zip_safe=False,
 )

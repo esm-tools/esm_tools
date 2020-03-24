006ca94ca2fbcec6155fcfda6a56dc097deb8830
Dirk Barbi <dirk.barbi@awi.de>
2020-03-24 15:08:42 +0100

    Bump version: 3.1.4 → 3.1.5

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://gitlab.awi.de/esm_tools/esm_tools",
-    version="version='3.1.4'",
+    version="version='3.1.5'",
     zip_safe=False,
 )
 

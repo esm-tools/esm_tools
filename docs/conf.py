00152710b2abd03aacce5cf51e08910cd7063e8b
Paul Gierz <pgierz@awi.de>
2020-04-15 10:17:21 +0200

    move to rtd

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ import esm_master
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -83,19 +83,24 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    "style_nav_header_background": "white",
+    "logo_only": True,
+    "prev_next_buttons_location": "both",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_logo = "_static/ESM-TOOLS_LOGO_RGB_72dpi.jpg"
 
 # -- Options for HTMLHelp output ---------------------------------------
 

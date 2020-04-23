ee9ef69608b2a785514d60ad9e8a0c15c0380c84
Dirk Barbi <dirk.barbi@awi.de>
2020-04-23 11:52:16 +0200

    Bump version: 3.1.5 → 3.2.0

--- esm_master/__init__.py
+++ esm_master/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Dirk Barbi"""
 __email__ = 'dirk.barbi@awi.de'
-__version__ = '3.1.5'
+__version__ = '3.2.0'
 
 from . import database
--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dbarbi/esm_master',
-    version='3.1.5',
+    version='3.2.0',
     zip_safe=False,
 )

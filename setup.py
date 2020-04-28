c6b4ac095280ab300d4704472337d1287e35f4b9
Paul Gierz <pgierz@awi.de>
2020-04-28 08:37:37 +0200

    Bump version: 3.3.1 → 3.3.2

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://gitlab.awi.de/esm_tools/esm_tools",
-    version="3.3.1",
+    version="3.3.2",
     zip_safe=False,
 )
 

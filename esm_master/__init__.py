df532e610c523b461f9f8007c3c052cfc2ec9b0e
Paul Gierz <pgierz@awi.de>
2020-05-19 08:14:38 +0200

    Bump version: 4.0.0 → 4.0.1

--- esm_master/__init__.py
+++ esm_master/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Dirk Barbi"""
 __email__ = 'dirk.barbi@awi.de'
-__version__ = '4.0.0'
+__version__ = '4.0.1'
 
 from . import database
--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dbarbi/esm_master',
-    version='4.0.0',
+    version='4.0.1',
     zip_safe=False,
 )

cc417f1f6db2485db50b0adfa8dbf71b436accde
Paul Gierz <pgierz@awi.de>
2020-09-15 12:40:29 +0200

    Bump version: 4.2.2 → 4.2.3

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/esm_tools/esm_tools",
-    version="4.2.2",
+    version="4.2.3",
     zip_safe=False,
 )
 

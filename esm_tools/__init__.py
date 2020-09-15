acfba9f2f06a6e4976de6d23ebfc5c657e6290f0
Paul Gierz <pgierz@awi.de>
2020-09-15 14:47:58 +0200

    Bump version: 4.2.2 → 4.2.3

--- esm_tools/__init__.py
+++ esm_tools/__init__.py
@@ -22,7 +22,7 @@ so it's just the dictionary representation of the YAML.
 
 __author__ = """Dirk Barbi, Paul Gierz"""
 __email__ = "dirk.barbi@awi.de"
-__version__ = "4.2.2"
+__version__ = "4.2.3"
 
 import os
 import sys

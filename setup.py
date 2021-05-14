4c0c5b25fd9e7e958b7d2462e4f9551a005e1fec
Paul Gierz <pgierz@awi.de>
2021-05-14 12:12:36 +0200

    Update setup.py
    The setup.py url is incorrect.

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
-    url="https://github.com/esm_tools/esm_tools",
+    url="https://github.com/esm-tools/esm_tools",
     version="5.1.0",
     zip_safe=False,
 )

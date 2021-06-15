72da3f32f55c7bc8ad4e8047b2af639ac0f04de4
Miguel Andres-Martinez <miguel.andres-martinez@awi.de>
2021-06-15 20:17:56 +0200

    eternal runs working, at least with constant run lenghts. oifs.yaml has been cleaned up and generalized. New splitted preprocessing moved from awicm3.yaml to oifs.yaml. The preprocessing scripts now load the environment generated from esm_environment (machine switches deleted)

--- configs/components/oifs/change_icm_date.sh
+++ configs/components/oifs/change_icm_date.sh
@@ -19,41 +19,7 @@ ensemble_id=$9
 
 outdir=$indir
 
-if [[ "$(hostname -f)" =~ ollie ]] ; then
-    module purge
-    module load intel.compiler
-    module load cdo netcdf/4.6.2_intel
-    export PATH=/home/ollie/jstreffi/ecmwf/grib_api_intel_hdf5_1.10.2_gnu/bin:$PATH
-
-elif [[ "$(hostname -f)" =~ dkrz.de ]] ; then
-    export PATH=/sw/rhel6-x64/grib_api/grib_api-1.15.0-intel14/bin:$PATH
-    module purge
-    module load netcdf_c/4.3.2-gcc48
-    module load cdo
-
-elif [[ "$(hostname -f)" =~ hlrn.de ]] ; then
-	 module load intel/19.0.5 impi/2019.5
-	 export PATH=/home/shkifmsw/sw/HPC_libraries/intel2019.0.5_impi2019.5_20200811/bin:$PATH
-elif [[ "$(hostname -f)" =~ juwels ]] ; then
-	 
-    # new Intel 2019 settings 
-    # self compiled netcdf etc from Sebastian Wahl
-    module --force purge
-    module use $OTHERSTAGES
-    module load Stages/Devel-2019a
-    module load Intel/2019.5.281-GCC-8.3.0
-    module load ParaStationMPI/5.4.4-1-mt    
-    module load Python/3.6.8
-    module load imkl/2019.3.199
-    export IO_LIB_ROOT=/p/project/hirace/HPC_libraries/intel2019.5.281_parastation_5.4.4-1-mt_20201113/
-    export PATH=$IO_LIB_ROOT/bin:$PATH
-    export LD_LIBRARY_PATH=$IO_LIB_ROOT/lib:$LD_LIBRARY_PATH
-else
-   echo
-	echo $0 has not been adapted for $(hostname)
-	echo
-	exit 1
-fi 
+source $indir/../../scripts/env.sh
 
 echo " OpenIFS preprocessing "
 echo " ===================== "
@@ -70,13 +36,16 @@ echo " OutExp ID: $outexpid "
 echo " Start date: $startdate "
 echo " End date: $enddate "
 echo " Perturb: $perturb "
+# Miguel: Style is not supported anymore in this preprocessing scripts, I don't
+# understand why it's being removed. @JAN, @JOAKIM?
 echo " Style: $style"
 
-if [[ "x${style}" == "xjesus" ]] ; then
-   ndate=$(date -u -d "${inidate}" +%Y%m%d)
-else
-   ndate=$(date -u -d "${startdate}" +%Y%m%d)
-fi
+#if [[ "x${style}" == "xjesus" ]] ; then
+#   ndate=$(date -u -d "${inidate}" +%Y%m%d)
+#else
+#   ndate=$(date -u -d "${startdate}" +%Y%m%d)
+#fi
+ndate=$(date -u -d "${inidate}" +%Y%m%d)
 initime=$(date -u -d "${inidate}" +%Y-%m-%dT%T)
 starttime=$(date -u -d "${startdate}" +%Y-%m-%dT%T)
 endtime=$(date -u -d "${enddate}" +%Y-%m-%dT%T)
@@ -92,21 +61,21 @@ echo " $files "
 old=${indir}/ICMGG${inexpid}INIT
 new=${outdir}/ICMGG${outexpid}INIT
 newgginit=${new}
-if [ -f $old ]; then                                                                                                   
+if [ -f $old ]; then
     cp $old $old\_ori
-    rm $old                                              
+    rm $old
     grib_set -s dataDate=$ndate $old\_ori $new
-    echo " Made new file: " $new " with date " $ndate                                                                                                                 
-else                                                                                                                                                                 
-    echo " Could not find file " $old                                                                                                                                 
-    exit  
-fi 
+    echo " Made new file: " $new " with date " $ndate
+else
+    echo " Could not find file " $old
+    exit
+fi
 
 old=${indir}/ICMGG${inexpid}INIUA
 new=${outdir}/ICMGG${outexpid}INIUA
 if [ -f $old ]; then
     cp $old $old\_ori
-    rm $old  
+    rm $old
     grib_set -s dataDate=$ndate $old\_ori $new
     echo " Made new file: " $new " with date " $ndate
 else

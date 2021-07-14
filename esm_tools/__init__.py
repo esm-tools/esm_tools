3b570bb5cb2e6a00c161eb0a5a1f75d16e2b7dfc
Dirk Barbi <dirk.barbi@awi.de>
2021-07-14 11:40:50 +0200

    Bump version: 5.1.13 → 5.1.14

--- esm_tools/__init__.py
+++ esm_tools/__init__.py
@@ -22,7 +22,7 @@ so it's just the dictionary representation of the YAML.
 
 __author__ = """Dirk Barbi, Paul Gierz"""
 __email__ = "dirk.barbi@awi.de"
-__version__ = "5.1.13"
+__version__ = "5.1.14"
 
 import os
 import shutil

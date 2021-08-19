2b5d1b7effbed5ad880dd51a6ed1ce2fdb5b9b51
Miguel Andres-Martinez <miguel.andres-martinez@awi.de>
2021-08-19 11:04:56 +0200

    Bump version: 5.1.19 → 5.1.20

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/esm_tools/esm_tools",
-    version="5.1.19",
+    version="5.1.20",
     zip_safe=False,
 )
 

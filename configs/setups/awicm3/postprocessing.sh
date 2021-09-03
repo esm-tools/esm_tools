da770a68f3b57f8cfa50e074e5690bf3b85c3a26
Miguel Andres-Martinez <miguel.andres-martinez@awi.de>
2021-09-03 02:53:56 +0200

    workflow for postprocessing and dummy postprocessing script

---
+++ configs/setups/awicm3/postprocessing.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "============================"
+echo "AWICM3 POSTPROCESSING SCRIPT"
+echo "============================"
+echo ""
+echo "The environment is loaded, for example, here is the value of:"
+echo "- NETCDF_CXX_INCLUDE_DIRECTORIES: $NETCDF_CXX_INCLUDE_DIRECTORIES"
+echo "Current folder: $(pwd)"

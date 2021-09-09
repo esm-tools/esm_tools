37e1c174fe0c6ab722674108497abfe131f9be6d
Dirk Barbi <dirk.barbi@awi.de>
2021-09-09 15:18:45 +0200

    Bump version: 5.1.21 → 5.1.22

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/esm_tools/esm_tools",
-    version="5.1.21",
+    version="5.1.22",
     zip_safe=False,
 )
 

1d88b8fde17c0389428b37ba466bbedf1e932b89
Paul Gierz <pgierz@awi.de>
2021-09-23 10:07:06 +0200

    feat: setup file corrected

--- setup.py
+++ setup.py
@@ -12,20 +12,34 @@ with open("stuff/HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
-    "pyyaml>=5.1",
-    "esm_master @ git+https://github.com/esm-tools/esm_master.git",
-    "esm_runscripts @ git+https://github.com/esm-tools/esm_runscripts.git",
-    "esm_rcfile @ git+https://github.com/esm-tools/esm_rcfile.git",
-    "esm_version_checker @ git+https://github.com/esm-tools/esm_version_checker.git",
+    "Click>=7.0",
+    "PyGithub",
+    "colorama",
+    "coloredlogs",
+    "emoji",
+    "f90nml",
     "gfw-creator",
+    "gitpython",
+    "loguru",
+    "numpy",
+    "packaging",
+    "pandas",
+    "psutil",
+    "pyyaml",
+    "pyyaml>=5.1",
+    "questionary",
+    "six",
+    "sqlalchemy",
+    "tabulate",
+    "tqdm",
+    "typing_extensions>=3.10.0.0",
+    "xdgenvpy",
 ]
 
 setup_requirements = []
 
-test_requirements = []
+test_requirements = ["pyfakefs"]
 
-thisfolder = getenv("PWD")  # that somehow works, even though pip copies everything to
-# a temp folder... seemingly without changing the PWD var
 setup(
     author="Dirk Barbi",
     author_email="dirk.barbi@awi.de",
@@ -41,15 +55,38 @@ setup(
         "Programming Language :: Python :: 3.8",
     ],
     description="ESM Tools external infrastructure for Earth System Modelling",
+    entry_points={
+        "console_scripts": [
+            "esm_archive=esm_archiving.cli:main",
+            "esm_cleanup=esm_cleanup.cli:main",
+            "esm_database=esm_database.cli:main",
+            "esm_master=esm_master.cli:main",
+            "esm_plugins=esm_plugin_manager.cli:main",
+            "esm_runscripts=esm_runscripts.cli:main",
+            "esm_tests=esm_tests.cli:main",
+            "esm_utilities=esm_utilities.cli:main",
+            "esm_versions=esm_version_checker.cli:main",
+        ],
+    },
     install_requires=requirements,
     license="GNU General Public License v2",
     long_description=readme + "\n\n" + history,
     include_package_data=True,
     keywords="esm_tools",
     name="esm_tools",
-    packages=["esm_tools", "esm_tools.configs", "esm_tools.namelists", "esm_tools.runscripts"],
-    package_dir={'esm_tools.configs': "configs", "esm_tools.namelists": "namelists", "esm_tools.runscripts": "runscripts"},
-    package_data={'esm_tools.configs': ['../configs/*'], "esm_tools.namelists": ["../namelists/*"], "esm_tools.runscripts": ["../runscripts/*"]},
+    packages=find_packages("src")
+    + ["esm_tools.configs", "esm_tools.namelists", "esm_tools.runscripts"],
+    package_dir={
+        "": "src",
+        "esm_tools.configs": "configs",
+        "esm_tools.namelists": "namelists",
+        "esm_tools.runscripts": "runscripts",
+    },
+    package_data={
+        "esm_tools.configs": ["../configs/*"],
+        "esm_tools.namelists": ["../namelists/*"],
+        "esm_tools.runscripts": ["../runscripts/*"],
+    },
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
@@ -57,4 +94,3 @@ setup(
     version="5.1.21",
     zip_safe=False,
 )
-

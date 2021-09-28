440220638f0315929807b4c3d938b47f5ba2aad2
Paul Gierz <pgierz@awi.de>
2021-09-28 13:53:40 +0200

    fix(ci, style): style checker can't work as there is a syntax error

--- dataprocessings/vilma/ice_vilma_interface.py
+++ dataprocessings/vilma/ice_vilma_interface.py
@@ -2,7 +2,7 @@ def prepare_environment(config):
     environment_dict = {
             ICE_TO_VILMA: 1,
             BASE_DIR: config["general"]["base_dir"],
-            solidearth_ice_thickness_file: 
+            solidearth_ice_thickness_file: "/some/file"
             }
     return environment_dict
 

2e6def902a59cb557db863f750ece114f4b46cd8
Lars Ackermann <a270124@mlogin100.hpc.dkrz.de>
2021-10-08 20:50:18 +0200

    added freshwater hosing to compensate for salinity restoring

---
+++ runscripts/awicm-pism/coupling/utils/apply_hosing_correction.py
@@ -0,0 +1,43 @@
+import xarray as xr
+import sys
+import os.path
+
+rho_water = 1000 #kg/m3
+
+discharge_file = sys.argv[1]
+cell_area_echam_file = sys.argv[2]
+cell_area_fesom_file = sys.argv[3]
+hosing_dir = sys.argv[4]
+
+if os.path.exists(cell_area_fesom_file):
+    print(" * Loading total FESOM grid area from ", os.path.join(cell_area_fesom_file, "fesom.mesh.diag.nc"))
+    fl = xr.open_dataset(os.path.join(cell_area_fesom_file, "fesom.mesh.diag.nc"), engine="netcdf4")
+    print(" * Summing up FESOM grid areas")
+    total_FESOM_cell_area = fl.nod_area[0, :].sum().squeeze().values
+    print(" * Total cell area = ", total_FESOM_cell_area)
+else:
+    print(" * File does not exist!")
+    exit
+
+if os.path.exists(discharge_file):
+    print(" * Loading ice discharge from ", discharge_file)
+    fl1 = xr.open_dataset(discharge_file, engine="netcdf4")
+else:
+    print(" * File does not exist!")
+    exit
+
+if os.path.exists(discharge_file):
+    print(" * Loading total ECHAM grid area from ", cell_area_echam_file)
+    fl2 = xr.open_dataset(cell_area_echam_file, engine="netcdf4")
+    print(" * Integrate discharge over ECHAM grid")
+    # Convert kg/s into m3/s
+    discharge_tot = fl1.total_ice_mass_loss_flux.weighted(fl2.cell_area.squeeze()).sum() / rho_water
+    print(" * Distribute over FESOM domain evenly")
+    discharge = -discharge_tot / total_FESOM_cell_area
+else:
+    print(" * File does not exist!")
+    exit
+
+with open(os.path.join(hosing_dir, "landice_yearly_mass_loss.out"), 'w') as f:
+    f.write(str(1) + "\n" + str(discharge.squeeze().values))
+

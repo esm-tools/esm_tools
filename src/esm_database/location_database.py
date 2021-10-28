5c69a22ce0afcc1fdb690c33805f2748cd3457a6
Paul Gierz <pgierz@awi.de>
2021-10-28 10:44:06 +0200

    style(esm_database): runs black on esm_database

--- src/esm_database/location_database.py
+++ src/esm_database/location_database.py
@@ -8,13 +8,14 @@ main_database_file = os.path.expanduser("~") + "/.esm_tools/esmtools.db"
 if not os.path.isdir(os.path.expanduser("~") + "/.esm_tools"):
     os.mkdir(os.path.expanduser("~") + "/.esm_tools")
 
-engine = create_engine('sqlite:///' + main_database_file, echo = False)
+engine = create_engine("sqlite:///" + main_database_file, echo=False)
 base = declarative_base()
 
-class database_location(base):
-    __tablename__ = 'locations'
 
-    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+class database_location(base):
+    __tablename__ = "locations"
+
+    id = Column(Integer, Sequence("user_id_seq"), primary_key=True)
     table_name = Column(String)
     location = Column(String)
     class_in = Column(String)
@@ -23,21 +24,23 @@ class database_location(base):
     def topline():
         print()
         print("Databases:")
-        print('{0: >4}'.format('ID')                     + "   " + 
-                '{0: >17}'.format('table_name')          + "   " +
-                '{0: >60}'.format('location')            + "   " + 
-                '{0: >17}'.format('class_in')
-                )
+        print(
+            "{0: >4}".format("ID")
+            + "   "
+            + "{0: >17}".format("table_name")
+            + "   "
+            + "{0: >60}".format("location")
+            + "   "
+            + "{0: >17}".format("class_in")
+        )
 
     def __repr__(self):
-        return "%4s   %17s   %85s   %17s"  % (
-                str(self.id), 
-                str(self.table_name),
-                str(self.location),
-                str(self.class_in),
-                )
-
-
+        return "%4s   %17s   %85s   %17s" % (
+            str(self.id),
+            str(self.table_name),
+            str(self.location),
+            str(self.class_in),
+        )
 
 
 base.metadata.create_all(engine)
@@ -62,18 +65,14 @@ def register(table_name, given_location, class_in):
                     loc_id = location.id
                     action = "update"
                     break
-    
+
     if action == "new":
         new_loc = database_location(
-                table_name = table_name,
-                location = given_location,
-                class_in = class_in
-                )
+            table_name=table_name, location=given_location, class_in=class_in
+        )
         session.add(new_loc)
         session.commit()
     elif action == "update":
-        loc = session.query(database_location).filter_by(id = loc_id).first()
+        loc = session.query(database_location).filter_by(id=loc_id).first()
         loc.location = given_location
         session.commit()
-
-

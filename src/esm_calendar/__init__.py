b898ccebee0036ae77f781e54011ceb414a12a8d
Paul Gierz <pgierz@awi.de>
2021-10-28 10:40:45 +0200

    style(esm_calendar): runs black on esm_calendar

--- src/esm_calendar/__init__.py
+++ src/esm_calendar/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for ESM Calendar."""
 
 __author__ = """Dirk Barbi"""
-__email__ = 'dirk.barbi@awi.de'
+__email__ = "dirk.barbi@awi.de"
 __version__ = "5.1.21"
 
 from .esm_calendar import *

eb4b1e585a1228eba3213bba08d5dd739fe304b4
Paul Gierz <pgierz@awi.de>
2021-10-28 11:06:19 +0200

    style(esm_profile): runs black on esm_profile

--- src/esm_profile/esm_profile.py
+++ src/esm_profile/esm_profile.py
@@ -2,12 +2,17 @@ import time
 
 timing_info = []
 
+
 def timing(f):
     def wrap(*args):
         time1 = time.time()
         ret = f(*args)
         time2 = time.time()
-        timing_info.append('{:s} function took {:.3f} ms'.format(f.__qualname__, (time2-time1)*1000.0))
+        timing_info.append(
+            "{:s} function took {:.3f} ms".format(
+                f.__qualname__, (time2 - time1) * 1000.0
+            )
+        )
         return ret
-    return wrap
 
+    return wrap

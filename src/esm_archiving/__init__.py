0d8ffa7f795c219b15ea6c823fe1c059f92705cc
Miguel Andres-Martinez <miguel.andres-martinez@awi.de>
2021-12-21 11:48:18 +0100

    Bump version: 6.0.2 → 6.0.3

--- src/esm_archiving/__init__.py
+++ src/esm_archiving/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Paul Gierz"""
 __email__ = "pgierz@awi.de"
-__version__ = "6.0.2"
+__version__ = "6.0.3"
 
 from .esm_archiving import (
     archive_mistral,
--- src/esm_database/__init__.py
+++ src/esm_database/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Dirk Barbi"""
 __email__ = "dirk.barbi@awi.de"
-__version__ = "6.0.2"
+__version__ = "6.0.3"

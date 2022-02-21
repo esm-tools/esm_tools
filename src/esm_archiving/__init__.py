d98ecda6767544f1094c85c0a232ef816483ae56
Paul Gierz <pgierz@awi.de>
2022-02-21 14:07:19 +0100

    Bump version: 6.1.9 → 6.1.10

--- src/esm_archiving/__init__.py
+++ src/esm_archiving/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Paul Gierz"""
 __email__ = "pgierz@awi.de"
-__version__ = "6.1.9"
+__version__ = "6.1.10"
 
 from .esm_archiving import (
     archive_mistral,
--- src/esm_master/__init__.py
+++ src/esm_master/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Dirk Barbi"""
 __email__ = "dirk.barbi@awi.de"
-__version__ = "6.1.9"
+__version__ = "6.1.10"
 
 
 from . import database

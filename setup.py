bd41d14530254e8d5757303811af1559e016c24d
BumpVersion Action <bumpversion@github-actions>
2022-03-03 11:28:41 +0000

    Bump version: 6.2.0 → 6.3.0

--- setup.py
+++ setup.py
@@ -92,6 +92,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/esm-tools/esm_tools",
-    version="6.2.0",
+    version="6.3.0",
     zip_safe=False,
 )
--- src/esm_archiving/__init__.py
+++ src/esm_archiving/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Paul Gierz"""
 __email__ = "pgierz@awi.de"
-__version__ = "6.2.0"
+__version__ = "6.3.0"
 
 from .esm_archiving import (
     archive_mistral,

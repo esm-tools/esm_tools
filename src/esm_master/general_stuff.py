2a16501e01de6ada205bd9c7f6b031c6397bb228
Paul Gierz <pgierz@awi.com>
2022-03-28 10:03:47 +0200

    ci: secrets are confusing

--- src/esm_master/general_stuff.py
+++ src/esm_master/general_stuff.py
@@ -323,13 +323,15 @@ class version_control_infos:
                 if "gitlab.awi.de" in repo:
                     repo = repo.replace(
                         "gitlab.awi.de",
-                        f"{os.environ['GITLAB_AWI_TOKEN']}@gitlab.awi.de",
+                        f"{os.environ['GITLAB_AWI_USER_NAME']}@gitlab.awi.de",
                     )
                 elif "gitlab.dkrz.de" in repo:
                     repo = repo.replace(
                         "gitlab.dkrz.de",
-                        f"{os.environ['GITLAB_DKRZ_TOKEN']}@gitlab.dkrz.de",
+                        f"{os.environ['GITLAB_DKRZ_USER_NAME']}@gitlab.dkrz.de",
                     )
+                elif "github.com" in repo:
+                    print("No token needed for github.com repositories!")
                 else:
                     print(f"Sorry, no CI token defined for {repo}")
                     sys.exit(1)

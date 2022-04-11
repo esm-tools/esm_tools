1dcc4f6fbfe030febd4c3f8f9713d60a614f1f8b
Miguel Andres-Martinez <a270152@levante3.atos.local>
2022-04-11 12:45:04 +0200

    some more changes to get back taskset

--- src/esm_runscripts/batch_system.py
+++ src/esm_runscripts/batch_system.py
@@ -50,12 +50,10 @@ class batch_system:
     def add_pre_launcher_lines(self, config, cluster, runfile):
         return self.bs.add_pre_launcher_lines(config, cluster, runfile)
 
-    # TODO: remove it once it's not needed anymore (substituted by packjob)
     def write_het_par_wrappers(self, config):
         return self.bs.write_het_par_wrappers(config)
 
-    def het_par_headers(self, config, cluster, all_values_flat):
-        return self.bs.het_par_headers(config, cluster, all_values_flat)
+    # methods that actually do something
 
     def prepare_launcher(self, config, cluster):
         self.bs.prepare_launcher(config, cluster)
@@ -153,12 +151,6 @@ class batch_system:
                 all_values_flat.append(value)
             elif isinstance(value, list):
                 all_values_flat.extend(value)
-
-        # Call the ``het_par_headers`` method to calculate the heterogeneous
-        # parallelization flags, if necessary
-        all_values_flat = config["general"]["batch"].het_par_headers(
-            config, cluster, all_values_flat
-        )
 
         # loop over all batch flag values and replace the tags
         for value in all_values_flat:
@@ -449,12 +441,9 @@ class batch_system:
 
                 config = batch_system.calculate_requirements(config, cluster)
                 if cluster in reserved_jobtypes:
-                    # TODO: remove it once it's not needed anymore (substituted by packjob)
                     config = config["general"]["batch"].write_het_par_wrappers(config)
-                # Prepare launcher
-                config = config["general"]["batch"].prepare_launcher(config, cluster)
-                # Initiate the header
                 header = batch_system.get_batch_header(config, cluster)
+                config = config["general"]["batch"].prepare_launcher(config, cluster)
 
                 for line in header:
                     runfile.write(line + "\n")
@@ -677,9 +666,10 @@ class batch_system:
             if int(omp_num_threads) > 1 and model in config["general"].get(
                 "valid_model_names", []
             ):
+                config["general"]["heterogeneous_parallelization"] = True
                 config["computer"][
                     "heterogeneous_parallelization"
-                ] = True
+                ] = True  # dont like this
                 if (
                     not config[model].get("nproc", False)
                     and not config[model].get("nproca", False)
@@ -687,163 +677,6 @@ class batch_system:
                 ):
                     config[model]["nproc"] = 1
         return config
-
-    def het_par_launcher_lines(self, config, cluster):
-        """
-        Loops through the components to generate job launcher flags and execution
-        commands, to be appended in substitution to the ``@components@`` tag, in
-        ``computer.execution_command``, that would later be used in the writing of the
-        ``.run`` file, in ``batch_system.py``.
-
-        Parameters
-        ----------
-        config : dict
-            Configuration dictionary containing information about the experiment and
-            experiment directory.
-        cluster : str
-            Type of job cluster.
-        """
-        component_lines = []
-        # Read in the separator to be used in between component calls in the job
-        # launcher
-        sep = config["computer"].get("launcher_comp_sep", "\\\n    ") + " "
-        # Loop through the components
-        for model in config["general"]["valid_model_names"]:
-            command = None
-            # Read in execution command
-            if "execution_command" in config[model]:
-                command = config[model]["execution_command"]
-            elif "executable" in config[model]:
-                command = config[model]["executable"]
-            # Prepare the MPMD commands
-            if command:
-                launcher = config["computer"].get("launcher")
-                launcher_flags = self.calc_launcher_flags(config, model, cluster)
-                component_lines.append(f"{launcher_flags} ./{command} ")
-
-        # Merge each component flags and commands into a single string
-        components = sep.join(component_lines)
-        # Substitute the ``@components@`` tag for the final launch command
-        config["computer"]["execution_command"] = config["computer"][
-            "execution_command"
-        ].replace("@components@", components).replace("@jobtype@", cluster)
-
-    @staticmethod
-    def calc_launcher_flags(config, model, cluster):
-        """
-        Calculates the launcher flags for the job luncher based on the ``nproc`` of the
-        different components, with the possibility of using heterogeneous
-        parallelization (using both MPI and OpenMP).
-
-        Launcher flags will vary from job launcher to job launcher, and therefore, a
-        variable ``launcher_flags`` needs to be defined for each different launcher in
-        the ``pbs.yaml`` configuration file. This variable should contain the flags
-        themselves followed by their respective values to be calculated in this method
-        and substituted in their respective tags of the ``launcher_flags`` variable.
-        This method supports the following variables:
-
-            - ``@nproc@``: total number of MPI tasks for the component taken from
-              ``<component>.nproc``.
-            - ``@nproc_per_node@``: number of MPI tasks per node calculated from
-              the number of number of CPUs per MPI task (``<component>.cpus_per_proc``),
-              and the number of cores per node in the machine
-              (``computer.cores_per_node``).
-            - ``@cpus_per_proc@``: number of CPUs per MPI task to be used
-              (``<component>.cpus_per_proc``). If not defined, it takes the value of
-              ``<component>.omp_num_threads``. This variables are not necessarily the
-              same, to allow the use of  more cores per MPI task than OpenMP threads,
-              if necessary.
-            - ``@omp_num_threads@``: number of OpenMP threads per MPI task
-              (``<component>.omp_num_threads``). Its default value is ``1``, if not
-              defined. The number of OpenMP threads per task cannot be larger than the
-              number of CPUs per task.
-
-        Parameters
-        ----------
-        config : dict
-            Configuration dictionary containing information about the experiment and
-            experiment directory.
-        model : str
-            Component for which the flags are to be calculated.
-        cluster : str
-            Type of job cluster.
-
-        Returns
-        -------
-        launcher_flags : str
-            Launcher flags string with the calculated numbers for the ``model`` already
-            substituted in the tags.
-        """
-        launcher = config["computer"]["launcher"]
-        launcher_flags = config["computer"]["launcher_flags_per_component"]
-        # Cores per node
-        # cores_per_node = config["computer"]["cores_per_node"]
-        if cluster == "compute":
-            cores_per_node = config["computer"]["partitions"]["compute"][
-                "cores_per_node"
-            ]
-        else:
-            cores_per_node = config["computer"]["partitions"]["pp"][
-                "cores_per_node"
-            ]
-        # Get the OMP number of threads
-        omp_num_threads = config[model].get("omp_num_threads", 1)
-
-        if "nproc" in config[model]:
-            # aprun flags commented following the conventions in p. 14 of the ALEPH ppt
-            # manual day_1.session_2.advanced_use_of_aprun.ppt
-
-            # Total number of PEs (MPI-ranks) (e.g. aprun -n)
-            nproc = config[model]["nproc"]
-            # CPUs per MPI-rank (e.g. aprun -d)
-            cpus_per_proc = config[model].get("cpus_per_proc", omp_num_threads)
-            # Check for CPUs and OpenMP threads
-            if omp_num_threads > cpus_per_proc:
-                esm_parser.user_error(
-                    "OpenMP configuration",
-                    (
-                        "The number of OpenMP threads cannot be larger than the number"
-                        + "of CPUs per MPI task requested. Your values:\n"
-                        + f"    {model}.omp_num_threads: {omp_num_threads}\n"
-                        + f"    {model}.cpus_per_proc: {cpus_per_proc}\n"
-                    ),
-                )
-        elif "nproca" in config[model] and "procb" in config[model]:
-            # ``nproca``/``nprocb`` not compatible with ``omp_num_threads``
-            if omp_num_threads > 1:
-                esm_parser.user_note(
-                    "nproc",
-                    "``nproca``/``nprocb`` not compatible with ``omp_num_threads``",
-                )
-            nproc = config[model]["nproca"] * config[model]["nprocb"]
-            cpus_per_proc = 1
-            omp_num_threads = 1
-
-        # Number of nodes needed
-        nodes = int(nproc * cpus_per_proc / cores_per_node) + (
-            (nproc * cpus_per_proc) % cores_per_node > 0
-        )
-        config[model]["nodes"] = nodes
-
-        # PEs (MPI-ranks) per compute node (e.g. aprun -N)
-        nproc_per_node = int(nproc / nodes)
-
-        # Replace tags in the laucher flags
-        replacement_tags = [
-            ("@nnodes@", nodes),
-            ("@nproc@", nproc),
-            ("@nproc_per_node@", nproc_per_node),
-            ("@cpus_per_proc@", cpus_per_proc),
-            ("@omp_num_threads@", omp_num_threads),
-        ]
-        # Replace all tags
-        for (tag, repl) in replacement_tags:
-            launcher_flags = launcher_flags.replace(tag, str(repl))
-        # Substitute @MODEL@ with the model name
-        launcher_flags = launcher_flags.replace("@MODEL@", model.upper())
-
-        return launcher_flags
-
 
 
 def submits_another_job(config, cluster):

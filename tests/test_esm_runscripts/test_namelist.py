7132a937b12f38fc0d040806809d82c691367f07
mandresm <miguel.andres-martinez@awi.de>
2022-06-24 17:06:09 +0200

    unit tests for this fix

--- tests/test_esm_runscripts/test_namelist.py
+++ tests/test_esm_runscripts/test_namelist.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
 """ Tests for ``esm_runscripts.namelist``"""
-import unittest
-import tempfile
+import os
 import shutil
+import subprocess
+import sys
+import tempfile
+import unittest
 import yaml
+
+from io import StringIO
 
 from esm_runscripts import namelists
 import esm_tools
@@ -22,6 +27,18 @@ TEST_NML_PATH = (
 )
 
 
+class Capturing(list):
+    """Taken from https://stackoverflow.com/questions/16571150/how-to-capture-stdout-output-from-a-python-function-call"""
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio    # free up some memory
+        sys.stdout = self._stdout
+
+
 class TestNamelist(unittest.TestCase):
     """Test the various static methods of Namelist"""
 
@@ -29,9 +46,11 @@ class TestNamelist(unittest.TestCase):
         """Prepares everything to might be needed"""
         self.config = yaml.safe_load(CONFIG_YAML)
         self.mconfig = self.config["echam"]
+        self._nml_name = self.mconfig["namelists"][0]
         self._thisrun_config_dir = tempfile.mkdtemp()
         self.mconfig["thisrun_config_dir"] = self._thisrun_config_dir
-        print(self.mconfig)
+        self._thisrun_namelist_path = os.path.join(self._thisrun_config_dir, self._nml_name)
+        shutil.copy2(TEST_NML_PATH, self._thisrun_namelist_path)
 
     def tearDown(self):
         shutil.rmtree(self._thisrun_config_dir)
@@ -41,3 +60,24 @@ class TestNamelist(unittest.TestCase):
         config = namelists.Namelist.nmls_load(self.mconfig)
         print(config)
         assert config
+
+    def test_nmls_syntax(self):
+        """Checks that the syntax errors are catched and reported to the user"""
+        # Break the namelist by removing the last line
+        with open(self._thisrun_namelist_path, "r") as f:
+            lines = f.readlines()
+        with open(self._thisrun_namelist_path, "w") as f:
+            for line in lines[:-1]:
+                f.write(line)
+        # Captures output (i.e. the user-friendly error)
+        with Capturing() as output:
+            try:
+                namelists.Namelist.nmls_load(self.mconfig)
+            except SystemExit as e:
+                error = e
+
+        # error needs to exist as the namelist has incorrect syntax
+        assert isinstance(error, SystemExit)
+        for line in output:
+            print(line)
+        assert any(["ERROR: Namelist format" in line for line in output])

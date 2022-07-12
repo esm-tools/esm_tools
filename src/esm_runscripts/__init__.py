fb9d1bf601c1aebc7752d7254051938fb4bc5a36
BumpVersion Action <bumpversion@github-actions>
2022-07-12 09:56:46 +0000

    Bump version: 6.11.5 → 6.12.0

--- src/esm_runscripts/__init__.py
+++ src/esm_runscripts/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Dirk Barbi"""
 __email__ = "dirk.barbi@awi.de"
-__version__ = "6.11.5"
+__version__ = "6.12.0"
 
 from .sim_objects import *
 from .batch_system import *
--- src/esm_utilities/__init__.py
+++ src/esm_utilities/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Paul Gierz"""
 __email__ = "pgierz@awi.de"
-__version__ = "6.11.5"
+__version__ = "6.12.0"
 
 from .utils import *

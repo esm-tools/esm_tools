6bf2611216d95775d681d49f17372c343177f886
BumpVersion Action <bumpversion@github-actions>
2022-07-20 18:52:26 +0000

    Bump version: 6.12.8 → 6.13.0

--- src/esm_archiving/__init__.py
+++ src/esm_archiving/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Paul Gierz"""
 __email__ = "pgierz@awi.de"
-__version__ = "6.12.8"
+__version__ = "6.13.0"
 
 from .esm_archiving import (
     archive_mistral,
--- src/esm_parser/__init__.py
+++ src/esm_parser/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Dirk Barbi"""
 __email__ = "dirk.barbi@awi.de"
-__version__ = "6.12.8"
+__version__ = "6.13.0"
 
 
 from .yaml_to_dict import yaml_file_to_dict

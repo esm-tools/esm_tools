66bb5a9b963845e2b6e72f217565d8a2e200e812
Paul Gierz <pgierz@awi.de>
2022-08-04 10:04:58 +0200

    filedicts: unit test example

---
+++ src/esm_runscripts/filedicts.py
@@ -0,0 +1,10 @@
+"""
+The file-dictionary implementation
+"""
+
+
+def copy_files(config):
+    """Copies files"""
+    # PG: No. We do not want this kind of general function. This is just to
+    # demonstrate how the test would work
+    return config

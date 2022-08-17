0138b523b833b12d3d21a4ce1415d3c00f730c54
Paul Gierz <pgierz@awi.de>
2022-08-17 16:24:56 +0200

    Update tests/test_esm_runscripts/test_filedicts.py
    Co-authored-by: Deniz Ural <51913535+denizural@users.noreply.github.com>

--- tests/test_esm_runscripts/test_filedicts.py
+++ tests/test_esm_runscripts/test_filedicts.py
@@ -741,5 +741,5 @@ def test_fname_has_date_stamp_info():
     fname = "blah_2000-01-01.nc"
     fname2 = "blah_2000-01.nc"
     date = esm_calendar.Date("2000-01-01T00:00:00")
-    assert filedicts._fname_has_date_stamp_info(fname, date) is True
+    assert filedicts._fname_has_date_stamp_info(fname, date)
     assert filedicts._fname_has_date_stamp_info(fname2, date) is False

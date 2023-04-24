3804549791d766bd67539cff13c8c3b1f205fc29
Miguel <63242832+mandresm@users.noreply.github.com>
2023-04-24 09:04:10 +0200

    Update src/esm_calendar/esm_calendar.py
    Co-authored-by: Paul Gierz <pgierz@awi.de>

--- src/esm_calendar/esm_calendar.py
+++ src/esm_calendar/esm_calendar.py
@@ -926,4 +926,4 @@ class Date(object):
         interval = [Date(date) for date in interval]
 
         # Check whether the ``self`` date falls within the interval
-        return self > interval[0] and self < interval[1]
+        return interval[0] < self < interval[1]

1dd7de11aff9f23f2d5bd2e250deb88de190f2dc
Miguel <63242832+mandresm@users.noreply.github.com>
2023-05-02 16:53:46 +0200

    Update tests/test_esm_calendar/test_esm_calendar.py
    Co-authored-by: Paul Gierz <pgierz@awi.de>

--- tests/test_esm_calendar/test_esm_calendar.py
+++ tests/test_esm_calendar/test_esm_calendar.py
@@ -9,7 +9,7 @@ import esm_calendar
 def test_date_is_in_interval():
     date1 = esm_calendar.Date("1949-12-31")
     date2 = esm_calendar.Date("1950-01-01")
-    interval = "1850-01-01->1950-01-01"
+    interval = ("1850-01-01", "1950-01-01")
 
     assert date1.is_in_interval(interval)
     assert date2.is_in_interval(interval) == False

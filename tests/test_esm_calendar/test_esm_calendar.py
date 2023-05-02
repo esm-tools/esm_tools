11d4812c8984af5df408001c13b221e5b28ced5c
Miguel Andres-Martinez <miguel.andres-martinez@awi.de>
2023-05-02 17:01:01 +0200

    add some changes for @pgierz suggestions

--- tests/test_esm_calendar/test_esm_calendar.py
+++ tests/test_esm_calendar/test_esm_calendar.py
@@ -7,9 +7,12 @@ import pytest
 import esm_calendar
 
 def test_date_is_in_interval():
+    """
+    Checks that a date falls into an interval while another doesn't
+    """
     date1 = esm_calendar.Date("1949-12-31")
     date2 = esm_calendar.Date("1950-01-01")
-    interval = ("1850-01-01", "1950-01-01")
+    interval = (esm_calendar.Date("1850-01-01"), esm_calendar.Date("1950-01-01"))
 
     assert date1.is_in_interval(interval)
     assert date2.is_in_interval(interval) == False

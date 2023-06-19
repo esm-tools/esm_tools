00e6df28225d1ebf7c3e021e9f246b3dddf52321
BumpVersion Action <bumpversion@github-actions>
2023-06-19 12:31:03 +0000

    Bump version: 6.21.15 → 6.21.16

--- src/esm_parser/__init__.py
+++ src/esm_parser/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Dirk Barbi"""
 __email__ = "dirk.barbi@awi.de"
-__version__ = "6.21.15"
+__version__ = "6.21.16"
 
 
 from .esm_parser import *
--- src/esm_utilities/__init__.py
+++ src/esm_utilities/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Paul Gierz"""
 __email__ = "pgierz@awi.de"
-__version__ = "6.21.15"
+__version__ = "6.21.16"
 
 from .utils import *

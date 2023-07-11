ba9828b59707030ef28d902e439f54a191431bff
Jan Streffing <jan.streffing@awi.de>
2023-07-11 11:51:03 +0200

    scripts for last 7 years of hist ensemble

--- runscripts/awicm3/v3.1/submit_hist_ensemble.sh
+++ runscripts/awicm3/v3.1/submit_hist_ensemble.sh
@@ -1,5 +1,5 @@
-esm_runscripts hist1-TCO159L91-CORE2.yaml -e hist1 --contained-run
-esm_runscripts hist2-TCO159L91-CORE2.yaml -e hist2 --contained-run
-esm_runscripts hist3-TCO159L91-CORE2.yaml -e hist3 --contained-run
-esm_runscripts hist4-TCO159L91-CORE2.yaml -e hist4 --contained-run
-esm_runscripts hist5-TCO159L91-CORE2.yaml -e hist5 --contained-run
+esm_runscripts hist1-TCO159L91-CORE2.yaml -e hist1 --contained-run -U
+esm_runscripts hist2-TCO159L91-CORE2.yaml -e hist2 --contained-run -U
+esm_runscripts hist3-TCO159L91-CORE2.yaml -e hist3 --contained-run -U
+esm_runscripts hist4-TCO159L91-CORE2.yaml -e hist4 --contained-run -U
+esm_runscripts hist5-TCO159L91-CORE2.yaml -e hist5 --contained-run -U

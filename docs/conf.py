b022c5df4f000cbb551b72a764f6db4a4dfdf11f
Nadine Wieters <nadine.wieters@awi.de>
2023-10-17 15:54:56 +0200

    Add language en in sphinxs conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -159,7 +159,7 @@ version = "{0}.{1}".format(version[0], version[1])
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.

48800beaf7f72fd0dfd5a23181d649f9b0a131d6
mandresm <miguel.andres-martinez@awi.de>
2023-12-18 09:05:25 +0100

    fix a bug on _get_real_dir_from_pth_file not getting the correct paths for direnv
    (cherry picked from commit 618c6c50b9f030afb06eeff3e4aed740ee9f369d)

--- src/esm_tools/__init__.py
+++ src/esm_tools/__init__.py
@@ -96,6 +96,18 @@ def _get_real_dir_from_pth_file(subfolder):
         [],
     )
     logger.debug(site_packages_dirs)
+
+    # Check if the user is running with direnv and removes all other sites
+    direnv = os.getenv("VIRTUAL_ENV")
+    if direnv:
+        logger.debug(f"User running with direnv, removing other sites")
+        site_package_dirs_new = []
+        for site_package_dir in site_packages_dirs:
+            if direnv in site_package_dir:
+                site_package_dirs_new.append(site_package_dir)
+        site_packages_dirs = site_package_dirs_new
+        logger.debug(site_packages_dirs)
+
     for site_package_dir in site_packages_dirs:
         logger.debug(f"Working on {site_package_dir}")
         # Read the pth file:
@@ -374,6 +386,7 @@ def get_config_filepath(config=""):
         cpath = _get_config_filepath_editable_install(config)
     else:
         cpath = _get_config_filepath_standard_install(config)
+    print(cpath)
     return cpath
 
 

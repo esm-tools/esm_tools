1d0e0fb007007fcc18a2f1a37533088c7dd2b235
Lars Ackermann <a270124@levante6.lvt.dkrz.de>
2024-01-03 15:29:21 +0100

    adapt installation scripts to albedo and levante

--- couplings/utils/install_calnoro.sh
+++ couplings/utils/install_calnoro.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-                
-module load gcc/12.1.0
+
+MACHINE=$1
+
 fortran_compiler=gfortran
-nc_config=/albedo/soft/sw/spack-sw/netcdf-fortran/4.5.4-yb7woqz/bin/nf-config
+
+if [[ ${MACHINE} == "albedo" ]]; then
+    module load gcc/12.1.0
+    nc_config=/albedo/soft/sw/spack-sw/netcdf-fortran/4.5.4-yb7woqz/bin/nf-config
+elif [[ ${MACHINE} == "levante" ]]; then
+    module load gcc/11.2.0-gcc-11.2.0 
+    nc_config=/sw/spack-levante/netcdf-fortran-4.5.3-pywf2l/bin/nf-config
+else
+    echo " * Unknown machine! No installation possible"
+    exit
+fi
+
 NETCDF_LIB=$($nc_config --flibs)
 NETCDF_INCLUDE=-I$($nc_config --includedir)
 

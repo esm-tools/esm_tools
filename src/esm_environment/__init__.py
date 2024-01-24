4bc748db08887fc07a8ba4b6889dcbdb4db0c469
BumpVersion Action <bumpversion@github-actions>
2024-01-24 07:40:55 +0000

    Bump version: 6.27.0 → 6.27.1

--- src/esm_environment/__init__.py
+++ src/esm_environment/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Dirk Barbi"""
 __email__ = "dirk.barbi@awi.de"
-__version__ = "6.27.0"
+__version__ = "6.27.1"
 
 from .esm_environment import *
--- src/esm_utilities/__init__.py
+++ src/esm_utilities/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Paul Gierz"""
 __email__ = "pgierz@awi.de"
-__version__ = "6.27.0"
+__version__ = "6.27.1"
 
 from .utils import *

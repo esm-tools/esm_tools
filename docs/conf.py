467bbc1594a8fddac1fe84a36085fdb1b14bc4f3
Nadine Wieters <nadine.wieters@awi.de>
2024-01-26 10:21:31 +0100

    Try to fix module error during sphinx doc build.

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('../src/'))
+sys.path.append('esm-tools/')
 print(os.path.abspath('../src'))
 print(sys.path)
 

4e6f44e272c400148934e913e44669eb6d3aa77a
Miguel Andres-Martinez <miguel.andres-martinez@awi.de>
2024-02-01 21:33:58 +0100

    Bump version: 6.27.3 → 6.27.4

--- src/esm_parser/__init__.py
+++ src/esm_parser/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Dirk Barbi"""
 __email__ = "dirk.barbi@awi.de"
-__version__ = "6.27.3"
+__version__ = "6.27.4"
 
 
 from .esm_parser import *
--- src/esm_utilities/__init__.py
+++ src/esm_utilities/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Paul Gierz"""
 __email__ = "pgierz@awi.de"
-__version__ = "6.27.3"
+__version__ = "6.27.4"
 
 from .utils import *

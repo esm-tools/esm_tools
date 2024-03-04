2bc1977299935292d354aee17eb868094680da7c
Miguel Andres-Martinez <miguel.andres-martinez@awi.de>
2024-03-04 15:40:37 +0100

    provisionally removing the conflicting packages

--- setup.py
+++ setup.py
@@ -36,9 +36,9 @@ requirements = [
     "xdgenvpy==2.3.5",
     "pydantic==1.10.2",
     "h5netcdf>=0.8.1",
+    "numexpr",
 #MA: Provisionally removing this packages to look at the automatic tests
-#    "powerlaw",
-#    "numexpr",
+     #"powerlaw",
 #    "pyfesom2",
 ]
 

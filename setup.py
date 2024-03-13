9dbd7d00026560432af5e0f001102ca88d569e81
BumpVersion Action <bumpversion@github-actions>
2024-03-13 08:26:27 +0000

    Bump version: 6.28.0 → 6.29.0

--- setup.py
+++ setup.py
@@ -105,6 +105,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/esm-tools/esm_tools",
-    version="6.28.0",
+    version="6.29.0",
     zip_safe=False,
 )
--- src/esm_motd/__init__.py
+++ src/esm_motd/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Dirk Barbi"""
 __email__ = "dirk.barbi@awi.de"
-__version__ = "6.28.0"
+__version__ = "6.29.0"
 
 from .esm_motd import *

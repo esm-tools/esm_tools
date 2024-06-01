40f2f8896988b39188998284b6944c7fb92ae957
BumpVersion Action <bumpversion@github-actions>
2024-06-01 08:57:00 +0000

    Bump version: 6.33.0 → 6.33.1

--- src/esm_tools/__init__.py
+++ src/esm_tools/__init__.py
@@ -23,7 +23,7 @@ so it's just the dictionary representation of the YAML.
 
 __author__ = """Dirk Barbi, Paul Gierz"""
 __email__ = "dirk.barbi@awi.de"
-__version__ = "6.33.0"
+__version__ = "6.33.1"
 
 import functools
 import inspect

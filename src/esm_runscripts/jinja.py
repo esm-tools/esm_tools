acabaa1cbedf2a4fbaf970e02fc814aaab26b3ff
Miguel Andres-Martinez <miguel.andres-martinez@awi.de>
2024-09-28 18:25:30 +0200

    add unit testing and removal of j2 extension for the target

--- src/esm_runscripts/jinja.py
+++ src/esm_runscripts/jinja.py
@@ -1,15 +1,41 @@
 import os
 
-import esm_parser
-
-from jinja2 import Template, StrictUndefined, UndefinedError
+from jinja2 import StrictUndefined, Template, UndefinedError
 from loguru import logger
+
+import esm_parser
 
 
 def render_template(config, source, target):
+    """
+    Renders a ``target`` file using a ``source`` template. ``config`` is used to
+    substitute the place holders in the template. Like in any other ninja template
+    it's possible to use the shorten version (``{{xios.ni_glo}}``) or following the
+    python dictionary syntax (``{{xios['ni_glo']}}``).
+
+    Parameters
+    ----------
+    config : dict
+        Dictionary with the configuration data to be used in the template
+    source : str
+        Relative or absolute path to the template
+    target : str
+        Relative or absolute path to the rendered file
+
+    Raises
+    ------
+    user_error : esm_parser.user_error
+        If the parameter to be substituting does not exists in ``config``
+    """
+    # If target finishes with ``.j2``, remove it
+    if target.endswith(".j2"):
+        target = target[:-3]
+
+    # Read the template
     with open(source, "r") as f:
         template = Template(f.read(), undefined=StrictUndefined)
 
+    # Try to render the file using the data in ``config``
     try:
         content = template.render(config)
     except UndefinedError as e:
@@ -23,6 +49,7 @@ def render_template(config, source, target):
     if os.path.isfile(target):
         logger.debug(f"File already exists!: {target}")
 
+    # Write the rendered file
     with open(target, "w") as f:
         f.write(content)
 
---
+++ tests/test_esm_runscripts/test_jinja.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+""" Tests for ``esm_runscripts.jinja``"""
+import copy
+import sys
+import unittest
+from io import StringIO
+
+from esm_runscripts import jinja
+
+config = {
+    "xios": {
+        "ni_glo": 400,
+        "nj_glo": 600,
+    },
+}
+
+rendered_file = """<domain_definition>
+  <!-- Definition of the native domain of the model -->
+  <domain id="reduced_gaussian" long_name="reduced Gaussian grid" type="gaussian" />
+
+  <!-- Definition of regular Gaussian domains -->
+  <domain_group id="regular_domains" type="rectilinear" >
+    <domain id="regular" long_name="regular grid" ni_glo="400" nj_glo="600" >
+      <generate_rectilinear_domain />
+      <interpolate_domain order="1" write_weight="true" />
+    </domain>
+  </domain_group>
+</domain_definition>"""
+
+
+class Capturing(list):
+    """Taken from https://stackoverflow.com/questions/16571150/how-to-capture-stdout-output-from-a-python-function-call"""
+
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio  # free up some memory
+        sys.stdout = self._stdout
+
+
+class TestJinja(unittest.TestCase):
+    """Test the various static methods of the jinja implementation"""
+
+    def setUp(self):
+        """Prepares everything to might be needed"""
+        self.config = config
+
+    def test_jinja(self):
+        """Tests whether jinja is working correctly"""
+        jinja.render_template(self.config, "template.j2", "output.xml")
+
+        with open("output.xml", "r") as f:
+            output = f.read()
+
+        assert output == rendered_file
+
+    def test_jinja_remove_j2_extension(self):
+        """
+        Checks that the error is raised when the rendered file still contains the j2
+        extension
+        """
+        jinja.render_template(self.config, "template.j2", "output.xml.j2")
+
+        with open("output.xml", "r") as f:
+            output = f.read()
+
+        assert output == rendered_file
+
+    def test_jinja_variable_not_defined_error(self):
+        """
+        Checks that the non-defined variables in the template are catched and reported
+        to the user
+        """
+        cconfig = copy.deepcopy(self.config)
+        del cconfig["xios"]["ni_glo"]
+
+        with Capturing() as output:
+            try:
+                jinja.render_template(cconfig, "template.j2", "output.xml")
+            except SystemExit as e:
+                error = e
+
+        assert isinstance(error, SystemExit)
+        assert any(["ERROR: Jinja" in line for line in output])

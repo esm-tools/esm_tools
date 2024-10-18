36bd723e63d1a9249916c09dc836122059fdf4b3
Paul Gierz <pgierz@mac.com>
2024-10-18 19:26:00 +0200

    wip: archive

--- src/esm_tools/cli.py
+++ src/esm_tools/cli.py
@@ -8,12 +8,45 @@ import emoji
 import rich_click as click
 from click_repl import repl as crepl
 
+import esm_archiving.cli
 import esm_cleanup.cli
+import esm_master
+import esm_master.cli
 import esm_tools
 import esm_utilities.cli
 
-EPILOGUE = """The ESM-Tools Developer Team, 2017-2024"""
 VERSION = esm_tools.__version__
+EPILOGUE = f"Version {VERSION}, The ESM-Tools Developer Team, 2017-2024"
+
+click.rich_click.COMMAND_GROUPS = {
+    "esm-tools": [
+        {
+            "name": "esm-master Commands",
+            "commands": ["get", "conf", "comp"] + list(esm_master.get_meta_commands()),
+            "help": "Commands for downloading, compiling, and installing Models",
+            "table_styles": {"row_styles": ["red"]},
+        },
+        {
+            "name": "esm-runscripts Commands",
+            "commands": ["run", "clean", "archive"],
+            "help": "Commands for running and cleaning up experiments",
+            "table_styles": {"row_styles": ["green"]},
+        },
+        {
+            "name": "Utilities",
+            "commands": ["utils"],
+            "help": "General utilities",
+            "table_styles": {"row_styles": ["magenta"]},
+        },
+        {"name": "Cleanup", "commands": ["clean"], "help": "Cleanup utilities"},
+    ],
+    "esm-tools utils": [
+        {
+            "name": "Utility Commands",
+            "commands": ["logfile-stats"],
+        },
+    ],
+}
 
 
 @click.group(
@@ -25,13 +58,38 @@ VERSION = esm_tools.__version__
     version=VERSION,
     prog_name="esm-tools",
     message=emoji.emojize(
-        "%(prog)s :toolbox: v%(version)s\nRunning with Python :snake: v"
+        "%(prog)s :toolbox: %(version)s\nRunning with Python :snake: "
         + sys.version.replace("\n", " ")
     ),
 )
 def main(args=None):
     """Main Command Line Interface entry point"""
     return 0
+
+
+@main.command()
+@click.argument("name")
+def comp(name):
+    """Compile ESM components"""
+    esm_master.comp(name)
+
+
+@main.command()
+def get():
+    """Download ESM components"""
+    click.echo("Downloading ESM components")
+
+
+@main.command()
+def conf():
+    """Configure ESM components"""
+    click.echo("Configuring ESM components")
+
+
+@main.command()
+def install():
+    """Install ESM components"""
+    click.echo("Installing ESM components")
 
 
 @main.command()
@@ -46,6 +104,8 @@ def repl():
 
 main.add_command(esm_utilities.cli.main, name="utils")
 main.add_command(esm_cleanup.cli.main, name="clean")
+main.add_command(esm_master.cli.cli, name="master")
+main.add_command(esm_archiving.cli.main, name="archive")
 
 if __name__ == "__main__":
     sys.exit(main())

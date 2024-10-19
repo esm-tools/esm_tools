737521636fdc248b4edf34f47513b15974bb7b22
Paul Gierz <pgierz@mac.com>
2024-10-19 15:43:17 +0200

    wip

--- src/esm_master/cli.py
+++ src/esm_master/cli.py
@@ -65,14 +65,56 @@ def cli(
 
 def create_command(command_name, docstring=None):
     """Dynamically create command functions."""
+    # FIXME(PG): I don't like needing to decorate everything twice...
 
     @click.command(name=command_name, help=docstring)
-    @click.argument("arg", required=False, metavar="arg")
-    @click.pass_context
-    def command(ctx, arg=None):
-        options = ctx.obj
+    @click.argument("arg", default="", required=False, metavar="target")
+    @click.option(
+        "--check",
+        "-c",
+        is_flag=True,
+        help="Show what would be done, without making any changes.",
+    )
+    @click.option("--verbose", "-v", count=True, help="Toggle verbose mode.")
+    @click.option("--modify-config", "-m", default="", help="Modify configuration.")
+    @click.option("--ignore-errors", "-i", is_flag=True, help="Ignore errors.")
+    @click.option(
+        "--keep-task-script",
+        "-k",
+        is_flag=True,
+        help="Keep shell script generated to perform compilation/configuration jobs.",
+    )
+    @click.option(
+        "--generate-tab-complete", is_flag=True, help="Generate tab completion."
+    )
+    @click.option(
+        "--list-all-targets", is_flag=True, help="List all available targets."
+    )
+    @click.option("--no-motd", is_flag=True, help="Suppress the printing of MOTD.")
+    @click.version_option(version=__version__, help="Show the version and exit.")
+    def command(
+        arg,
+        check,
+        verbose,
+        modify_config,
+        ignore_errors,
+        keep_task_script,
+        generate_tab_complete,
+        list_all_targets,
+        no_motd,
+    ):
         target = f"{command_name}-{arg}" if arg else command_name
-        cli(target, **options)
+        cli(
+            target,
+            check,
+            verbose,
+            modify_config,
+            ignore_errors,
+            keep_task_script,
+            generate_tab_complete,
+            list_all_targets,
+            no_motd,
+        )
 
     return command
 

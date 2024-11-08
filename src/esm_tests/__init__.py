222a963f1fb8f4d43ed41e54af75f750a1a3f152
BumpVersion Action <bumpversion@github-actions>
2024-11-08 14:10:26 +0000

    Bump version: 6.42.2 → 6.43.0

--- src/esm_tests/__init__.py
+++ src/esm_tests/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Miguel Andres-Martinez"""
 __email__ = "miguel.andres-martinez@awi.de"
-__version__ = "6.42.2"
+__version__ = "6.43.0"
 
 from .initialization import *
 from .read_shipped_data import *

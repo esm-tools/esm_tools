82a85ba8f44d9774eae9825466f8ffde713ba78a
Paul Gierz <pgierz@awi.de>
2025-01-29 09:45:24 +0100

    chore: small typo

--- src/esm_runscripts/postprocess.py
+++ src/esm_runscripts/postprocess.py
@@ -20,6 +20,15 @@ def run_job(config):
     return config
 
 
+def convert_to_zarr(config):
+    if not config["general"].get("convert_to_zarr", False):
+        logger.info("Skipping conversion to zarr")
+        return config
+    logger.info("Converting output to zarr via xarray")
+
+    return config
+
+
 def _assemble_postprocess_tasks(config):
     """
     Generates all tasks for post processing which will be written to the run file.

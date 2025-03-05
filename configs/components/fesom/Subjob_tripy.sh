ccc6e006d926e9e0be79947c9c8bc11c5a911fff
Simon Porrmann <a270275@levante6.lvt.dkrz.de>
2025-03-05 11:41:29 +0100

    implementation of autotripy to FESOM2.6 config file

--- configs/components/fesom/Subjob_tripy.sh
+++ configs/components/fesom/Subjob_tripy.sh
@@ -1,46 +1,27 @@
 #!/bin/bash
-#Home directory and reset of variable
-cd ~
-source ~/.bash_profile
-
-cd ~
-
 # Function to check for py39 envirionmet. If necessary installing and activating.
-#this is just to not trigger errors while activating py39
-#module unload netcdf-c
-#module unload netcdf-fortran
 #Function for activating py39
+#NOTE: Installation of tripyview is not included yet
 activate_env() {
     cd ~
     source ~/.bash_profile
-    conda activate py39
+    conda activate esm-tools_auto_tripyview
 }
-
-cd /work/ab0995/a270275/runconfig
-# Check if conda ist installed
 if ! command -v conda &> /dev/null; then
-    echo "Conda is not installed. Plesa install conda or miniconda"
+    echo "Conda is not installed. Please install conda or miniconda"
     exit 1
 fi
 
-# Überprüfen, ob das Environment "py39" existiert
-if conda info --envs | grep -q "^py39 "; then
-    echo "Py39 is exisiting already. Activating now..."
+# Überprüfen, ob das Environment "esm-tools_auto_tripyview" existiert
+if conda info --envs | grep -q "^esm-tools_auto_tripyview "; then
+    echo "esm-tools_auto_tripyview is exisiting already. Activating now..."
     activate_env
 else
-    echo "The Py39 env is not exisiting, installing..."
-    cd~
+    echo "The esm-tools_auto_tripyview env is not exisiting, installing..."
+    cd ~
     source ~/.bash_profile
-    conda create -y --name py39 python=3.9
+    conda create -y --name esm-tools_auto_tripyview python=3.9
     activate_env
-fi
-
-#check if yq library is installed if not installing
-cd ~
-cd /work/ab0995/a270275/runconfig
-if ! command -v yq &> /dev/null; then
-    echo "Installing yq..."
-    pip install yq
 fi
 
 #import necessary variables from main config
@@ -50,6 +31,8 @@ next_date=$3
 initial_date=$4
 final_date=$5
 mesh_dir=$6
+base_dir=$7
+expid=$8
 
 #Change dates to years
 start_year=$(date -d "$start_date" +%Y)
@@ -63,7 +46,11 @@ export start_year
 export next_year
 export initial_year
 export final_year
+export base_dir
+export expid
 
+echo "$base_dir"
+echo "$expid"
 
 output_file="tripyrun_${start_year}.yaml"
 
@@ -71,17 +58,19 @@ export output_file
 
 #Create folder for Tripy configs and results in the experiment folder
 
-target_dir="$outdata_path/Tripyview/config"
+target_dir="${base_dir}/${expid}/analysis/Tripyview/config"
 if [ ! -d "$target_dir" ]; then
     echo "Directory $target_dir not existing. Creating..."
     mkdir -p "$target_dir"
 fi
 
 # go to Directory
-# Verwende envsubst, um den Platzhalter in der YAML-Datei zu ersetzen
+# working with envsubst (comes from GNU-gettext bib. Installed on OS and most linux distributions). Creating the Tripyview YAML for the current run.
+cd ${base_dir}/${expid}/config/fesom
 envsubst < ESM_auto_tripy.yaml > "$output_file"
+
 mv "$output_file" "$target_dir"
-#Run the new createt Tripy config
+#Run the new created Tripy config
 cd "$target_dir" 
 tripyrun "$output_file"
 

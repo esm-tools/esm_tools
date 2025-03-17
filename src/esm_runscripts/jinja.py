539130bf9e0ac09d900b1a01517090815ccd334d
Paul Gierz <pgierz@awi.de>
2025-03-17 09:24:43 +0100

    fix: better error handling for Jinja template errors

--- src/esm_runscripts/jinja.py
+++ src/esm_runscripts/jinja.py
@@ -1,6 +1,7 @@
 import os
 
-from jinja2 import StrictUndefined, Template, UndefinedError
+from jinja2 import (StrictUndefined, Template, TemplateSyntaxError,
+                    UndefinedError)
 from loguru import logger
 
 from esm_tools import user_error
@@ -44,6 +45,12 @@ def render_template(config, source, target):
             "Jinja",
             f"Error rendering template from ``{source}`` to ``{target}``. Variable "
             f"``{missing_variable}`` is not defined in any configuration file.",
+        )
+    except TemplateSyntaxError as e:
+        user_error(
+            "Jinja",
+            f"Error rendering template from ``{source}`` to ``{target}``. "
+            f"Syntax error: {e.message}",
         )
 
     if os.path.isfile(target):

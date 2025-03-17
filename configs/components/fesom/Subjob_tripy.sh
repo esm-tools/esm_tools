a07964e27709f831b6afad35008c28a591d1a2fc
Simon Porrmann <a270275@levante3.lvt.dkrz.de>
2025-03-17 14:05:24 +0100

    automatic installation of tripyview

--- configs/components/fesom/Subjob_tripy.sh
+++ configs/components/fesom/Subjob_tripy.sh
@@ -2,28 +2,6 @@
 # Function to check for py39 envirionmet. If necessary installing and activating.
 #Function for activating py39
 #NOTE: Installation of tripyview is not included yet
-activate_env() {
-    cd ~
-    source ~/.bash_profile
-    conda activate esm-tools_auto_tripyview
-}
-if ! command -v conda &> /dev/null; then
-    echo "Conda is not installed. Please install conda or miniconda"
-    exit 1
-fi
-
-# Überprüfen, ob das Environment "esm-tools_auto_tripyview" existiert
-if conda info --envs | grep -q "^esm-tools_auto_tripyview "; then
-    echo "esm-tools_auto_tripyview is exisiting already. Activating now..."
-    activate_env
-else
-    echo "The esm-tools_auto_tripyview env is not exisiting, installing..."
-    cd ~
-    source ~/.bash_profile
-    conda create -y --name esm-tools_auto_tripyview python=3.9
-    activate_env
-fi
-
 #import necessary variables from main config
 outdata_path=$1
 start_date=$2
@@ -51,6 +29,35 @@ export expid
 
 echo "$base_dir"
 echo "$expid"
+activate_env() {
+    source $(conda info --base)/etc/profile.d/conda.sh
+    conda activate esm-tools_auto_tripyview
+}
+if ! command -v conda &> /dev/null; then
+    echo "Conda is not installed. Please install conda or miniconda"
+    exit 1
+fi
+
+# Überprüfen, ob das Environment "esm-tools_auto_tripyview" existiert
+if conda info --envs | grep -q "^esm-tools_auto_tripyview "; then
+    echo "esm-tools_auto_tripyview is exisiting already. Activating now..."
+    activate_env
+else
+    echo "The esm-tools_auto_tripyview env is not exisiting, installing..."
+    source $(conda info --base)/etc/profile.d/conda.sh
+    conda create -y --name esm-tools_auto_tripyview python=3.9
+    activate_env
+    cd ${base_dir}/${expid}/src
+    git clone https://github.com/patrickscholz/tripyview
+    conda install -c conda-forge libstdcxx-ng
+    cd tripyview
+    pip install -e .
+    
+fi
+
+
+
+
 
 output_file="tripyrun_${start_year}.yaml"
 
@@ -66,7 +73,7 @@ fi
 
 # go to Directory
 # working with envsubst (comes from GNU-gettext bib. Installed on OS and most linux distributions). Creating the Tripyview YAML for the current run.
-cd ${base_dir}/${expid}/config/fesom
+cd ${base_dir}/${expid}/scripts/fesom
 envsubst < ESM_auto_tripy.yaml > "$output_file"
 
 mv "$output_file" "$target_dir"

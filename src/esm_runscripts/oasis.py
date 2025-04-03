c33514e38c1c443cf40b604e8a1eed5493d5bf3d
Jan Streffing <jan.streffing@awi.de>
2025-04-03 10:33:52 +0200

    test adding gconfig[oasis_copy_restart_overwrite]

--- src/esm_runscripts/oasis.py
+++ src/esm_runscripts/oasis.py
@@ -109,7 +109,7 @@ class oasis:
             else:
                 export_mode = "EXPORTED"
 
-        if bool(lresume) and not oasis_copy_restart_overwrite:
+        if bool(lresume) and not gconfig["oasis_copy_restart_overwrite"]:
             lag = str(0)
         else:
             lag = direction.get("lag", "0")

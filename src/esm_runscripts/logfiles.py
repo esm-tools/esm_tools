73fef695e6541cd7d6c6def81f49b1201993ca67
Miguel Andres-Martinez <miguel.andres-martinez@awi.de>
2025-08-11 16:11:03 +0200

    remove deprecated RuntimeLogger in favour of SmartSink + loguru.logger

--- src/esm_runscripts/logfiles.py
+++ src/esm_runscripts/logfiles.py
@@ -7,7 +7,6 @@ from . import helpers
 
 
 def initialize_logfiles(config, org_jobtype):
-    global logfile_handle
     global logfile_run_number
 
     logfile_run_number = str(config["general"]["run_number"])
@@ -31,15 +30,7 @@ def initialize_logfiles(config, org_jobtype):
                 "- start",
             ],
         )
-        logfile = RuntimeLogger(
-            config["general"]["logfile_path"],
-            "w",
-            buffering=1,
-        )
-    else:
-        logfile = sys.stdout
 
-    logfile_handle = logfile
     return config
 
 
@@ -60,8 +51,6 @@ def finalize_logfiles(config, org_jobtype):
             ],
         )
 
-    if not logfile_handle == sys.stdout:
-        logfile_handle.close()
     return config
 
 
@@ -84,76 +73,3 @@ def set_logfile_name(config, jobtype=None):
 
     return config
 
-
-class RuntimeLogger(_io.TextIOWrapper):
-    """
-    Logger object that takes care of both, writing the stdout and stderr to the
-    'mini-log' file corresponding to the different elements of the workflow (observe,
-    tidy, ...), and writing to the system stdout/stderr so that it is caught by SLURM
-    (or I guess PBS) and put into the ``*compute*.log`` specified in the ``.run`` file.
-    The ``*compute*.log`` file is then used to catch errors by ``observe``.
-
-    .. Note: This is a fast and minimal solution for release 6.0, but all this could be done
-    without needing this object, once ``loguru`` is implemented everywhere in some
-    point in the future.
-
-    .. Note: This is a subclass of ``_io.TextIOWrapper`` which is horrible! I could
-    have used simply ``object`` instead, but this was the only way I (MA) found for
-    ``f90nml`` package to be able to print namelists in the following lines of
-    ``namelist.py``::
-        def nmls_output(mconfig):
-            all_nmls = {}
-
-            for nml_name, nml_obj in mconfig.get("namelists", {}).items():
-                all_nmls[nml_name] = nml_obj  # PG: or a string representation?
-            for nml_name, nml in all_nmls.items():
-                message = f'\nFinal Contents of {nml_name}:'
-                print(message)
-                print(len(message) * '-')
-                nml.write(sys.stdout)  # Problematic line if the sys.stdout is not a file-like object
-                print('-' * 80)
-                print(f'::: end of the contents of {nml_name}\n')
-            return mconfig
-
-    Hopefully, we are not using any other methods than the ones specified here...
-    """
-
-    # FIXME(PG): This can be better.....
-
-    def __init__(self, *args, **kwargs):
-        """
-        Initializes the logger object, and opens the mini-log file, to be used as a
-        target for ``sys.stdout``::
-            sys.stdout = LogFile(args, kwargs)
-
-        Parameters
-        ----------
-        args, kwargs
-            Use the same parameters as you would with the ``open`` command.
-        """
-        # Direct system's stderr to stdout
-        sys.stderr = sys.stdout
-        # Store the system's stdout as an object
-        self.stdout = sys.stdout
-        # Open the mini-log file
-        self.file_obj = open(*args, **kwargs)
-
-    def write(self, *args, **kwargs):
-        """
-        Writes messages to the mini-log file and to the system's stdout.
-        """
-        # Write into the mini-log file
-        self.file_obj.write(*args, **kwargs)
-        # Write into the system's stdout (so that it makes it to *compute*.log defined
-        # in the .run file)
-        self.stdout.write(args[0])
-
-    def flush(self):
-        pass
-
-    def close(self, *args, **kwargs):
-        """
-        Closes the mini-log file.
-        """
-        # Close the mini-log file
-        self.file_obj.close(*args, **kwargs)

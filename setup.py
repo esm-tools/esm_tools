d80c21f72c4aec238bac2902104cabe54366187f
BumpVersion Action <bumpversion@github-actions>
2026-07-02 14:59:57 +0000

    Bump version: 6.65.0 → 6.65.1

--- setup.py
+++ setup.py
@@ -126,6 +126,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/esm-tools/esm_tools",
-    version="6.65.0",
+    version="6.65.1",
     zip_safe=False,
 )

9c198883409e12ea6b128d80598183434627a577
BumpVersion Action <bumpversion@github-actions>
2026-07-14 07:29:02 +0000

    Bump version: 6.65.1 → 6.66.0

--- setup.py
+++ setup.py
@@ -126,6 +126,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/esm-tools/esm_tools",
-    version="6.65.1",
+    version="6.66.0",
     zip_safe=False,
 )
--- src/esm_utilities/__init__.py
+++ src/esm_utilities/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Paul Gierz"""
 __email__ = "pgierz@awi.de"
-__version__ = "6.65.1"
+__version__ = "6.66.0"
 
 from .utils import *

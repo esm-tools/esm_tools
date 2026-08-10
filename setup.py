59858234f1ffd1fa3fbaa56f519e282527330573
BumpVersion Action <bumpversion@github-actions>
2026-08-10 14:47:04 +0000

    Bump version: 6.66.5 → 6.66.6

--- setup.py
+++ setup.py
@@ -129,6 +129,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/esm-tools/esm_tools",
-    version="6.66.5",
+    version="6.66.6",
     zip_safe=False,
 )
--- src/esm_parser/__init__.py
+++ src/esm_parser/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Dirk Barbi"""
 __email__ = "dirk.barbi@awi.de"
-__version__ = "6.66.5"
+__version__ = "6.66.6"
 
 
 from .dict_to_yaml import *
